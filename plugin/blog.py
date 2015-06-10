ef3a60a5fee149d113f69ceb6c426dd295af88c3
shell-chen <shell_chen@yeah.net>
2015-06-10 17:24:54 +0800

    add blog manage system

--- plugin/blog.py
+++ plugin/blog.py
@@ -7,26 +7,57 @@
 
 import os
 
-from flask import Blueprint, render_template
+from flask import render_template, request
+
+from flaskext import MyMethodView
+from setting import STOREPATH
+from publish import BlogData
 
 name = "blog"
-#bp = Blueprint("blog", __name__)
 bp = "this is bp"
+
+class Blog(MyMethodView):
+    def GET(self, key):
+        if not key:
+            return render_template("blog/index.html")
+        return "haha"
+
+class Article(MyMethodView):
+    def get(self, key):
+        if not key:
+            return render_template("blog/add.html")
+        return "haha"
+
+    def post(self):
+        form = request.form
+        title = form.get("title")
+        tags = form.get("tags")
+        if tags:
+            tags = tags.split(",")
+        else:
+            tags = [ ]
+
+        cls = form.get("cls","undefine")
+        context = form.get("context")
+        publish = form.get("publish", False)
+
+        bd = BlogData(STOREPATH)
+        bd.add(title, context, tags, cls, publish)
+
+        return "sucess!"
+
+def Articles(MyMethodView):
+    def get(self):
+        bd = BlogData(STOREPATH)
+        return
+
+
+
+
 urls = (
-        "/", "index",
-        "/test/", "test",
-        )
+        "/",           Blog,
+        "/article/",   Article,
+        "/articles/",  Articles,
+    )
 
-# @bp.route("/")
-# @bp.route("/index")
-# @bp.route("/me")
-# def blogindex():
-#     return render_template("blog/index.html")
 
-def index():
-    print "------"
-    return 'hello shell'
-
-def test():
-    print "------"
-    return 'hello'
--- sweb.py
+++ sweb.py
@@ -14,8 +14,9 @@ for p in sys_path:
 
 from flask import Flask, render_template
 import flask
-from plugins import load
+# from plugins import load
 from config import load_config
+from flaskext import Sunshine
 
 
 logging.basicConfig(level=logging.INFO,
@@ -23,94 +24,17 @@ logging.basicConfig(level=logging.INFO,
     #filename=".log",
     )
 
-
-class Sunshine(object):
-    plugin_dir = os.path.join(curdir, "plugin")
-    URLS = [ ]
-    PLUGINS = [ ]
-
-    def __init__(self, app):
-        self.app = app
-        self.load_plugin()
-        self.logurls()
-
-    def load_plugin(self):
-        mode_infos = load(self.plugin_dir)
-
-        for model, bps, name, urls in mode_infos.values():
-            self.init_mode(model, bps, name, urls)
-
-        self.__add_url()
-
-    def groups(self, urls):
-        i = 0
-        t = []
-        while i < len(urls):
-            t.append((urls[i], urls[i+1]))
-            i = i + 2
-        return t
-
-    def init_mode(self, model, bps, name, urls):
-        for url, fun in self.groups(urls):
-            if url.startswith("/"):
-                url = "/%s%s"%(name, url)
-            else:
-                url = os.path.join("/", name, url)
-
-            if isinstance(fun, basestring):
-                if not hasattr(model, fun):
-                    logging.error("plugin %s not hasattr [%s]"%(model, fun))
-                    continue
-                fun = getattr(model, fun)
-            self.URLS.append((url, fun))
-
-        self.PLUGINS.append((name, model))
-
-    def logurls(self):
-        l = 0
-        for u,c in self.URLS:
-            if len(u) > l:
-                l = len(u)
-
-        lines = ['']
-        for u,c in self.URLS:
-            lines.append("%s  ===>  %s" % (u.ljust(l),c))
-        logging.info('\n'.join(lines))
-
-    def __add_url(self):
-        for url, fun in self.URLS:
-            self.app.add_url_rule(url, None,fun)
-
-    def register_api(view, endpoint, url, pk='id', pk_type='int'):
-        view_func = view.as_view(endpoint)
-        app.add_url_rule(url, defaults={pk: None},
-                         view_func=view_func, methods=['GET',])
-        app.add_url_rule(url, view_func=view_func, methods=['POST',])
-        app.add_url_rule('%s<%s:%s>' % (url, pk_type, pk), view_func=view_func,
-                         methods=['GET', 'PUT', 'DELETE'])
-
-
-def init_app():
-    print flask.__file__
-    app = Flask(__name__)
-    init_model(app)
-    return app
-
-
-
-
 def run_application(app):
     app.run(host="0.0.0.0", port=8001, threaded=True, debug=True)
 
-def run_wsgi():
+def run_wsgi(app):
     from flup.server.fcgi import WSGIServer
     WSGIServer(app, bindAddress=('0.0.0.0', 8001), debug=True, multithreaded=True).run()
 
-# app = init_app()
-
 def sunshine_test():
+    plugin_dir = os.path.join(curdir, "plugin")
     app = Flask(__name__)
-    sunshine = Sunshine(app)
+    sunshine = Sunshine(app, plugin_dir)
     run_application(app)
 
 
